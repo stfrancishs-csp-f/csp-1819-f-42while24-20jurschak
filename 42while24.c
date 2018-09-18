8f2d5d54d1dfecde468c6cf83c614b1a8259beb3
Katelyn Jurschak <20jurschak@stfrancishs.org>
2018-09-18 17:21:43 +0000

    init

--- 42while24.c
+++ 42while24.c
@@ -4,19 +4,25 @@ int main(void)
     int sum=0;
     int k;
 
-    for (k=0;k<5;k++)
+    k=0;
+    while (k<5)
     {
-        sum+=k; //use something different in "while" version
+    printf("%2d\n",k);
+        sum=sum+k;
+        k++;
+
     }
+    //printf ("4 + 6 = %d -Sum\n",sum);
+    printf("sum-%d\n",sum);
 
-    printf ("%d\n",sum);
 
-    k=5;
+    for (k=11;k>1;)
+    {
+        k--;
+        printf ("%2d \n", k);
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
     }
+    printf ("Blastoff!!\n");
 
 }
 
